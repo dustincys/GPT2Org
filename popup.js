b232bcb9257ce26a793a8be0daf4fd26642fe158
yanshuo chu <yanshuoc@gmail.com>
2024-10-25 15:00:32 -0500

    Added 4 popup buttons for org templates

--- popup.js
+++ popup.js
@@ -7,15 +7,41 @@ const summaryTitle = document.getElementById("summaryTitle");
 
 const saveOrgBtn = document.getElementById("saveSummaryOrg");
 const saveRoamBtn = document.getElementById("saveSummaryRoam");
+const saveClockedBtn = document.getElementById("saveSummaryClocked");
+const saveJournalBtn = document.getElementById("saveSummaryJournal");
+
+
+saveJournalBtn.addEventListener("click", () => {
+    saveJournalBtn.disabled = true;
+
+    chrome.runtime.sendMessage({
+        "action": "saveJournal",
+        "content": summaryContent.textContent.replace(/\n/g, "%0A"),
+        "url": summaryURL.textContent,
+        "title": summaryTitle.textContent,
+    });
+});
+
+saveClockedBtn.addEventListener("click", () => {
+    saveClockedBtn.disabled = true;
+
+    chrome.runtime.sendMessage({
+        "action": "saveClocked",
+        "content": summaryContent.textContent.replace(/\n/g, "%0A"),
+        "url": summaryURL.textContent,
+        "title": summaryTitle.textContent,
+    });
+});
+
 
 saveOrgBtn.addEventListener("click", () => {
     saveOrgBtn.disabled = true;
 
     chrome.runtime.sendMessage({
-        action: "saveOrg",
-        content: summaryContent.textContent,
-        url: summaryURL.textContent,
-        title: summaryTitle.textContent,
+        "action": "saveOrg",
+        "content": summaryContent.textContent.replace(/\n/g, "%0A"),
+        "url": summaryURL.textContent,
+        "title": summaryTitle.textContent,
     });
 });
 
@@ -23,19 +49,20 @@ saveRoamBtn.addEventListener("click", () => {
     saveRoamBtn.disabled = true;
 
     chrome.runtime.sendMessage({
-        action: "saveRoam",
-        content: summaryContent.textContent,
-        url: summaryURL.textContent,
-        title: summaryTitle.textContent,
+        "action": "saveRoam",
+        "content": summaryContent.textContent.replace(/\n/g, "%0A"),
+        "url": summaryURL.textContent,
+        "title": summaryTitle.textContent,
     });
 });
 
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "apiRequestCompleted") {
+        console.log("received message!");
+        console.log(request);
         spinner.style.display = "none";
         if (request.success) {
-
             summaryContent.textContent = request.summary;
             summaryURL.textContent = request.url;
             summaryTitle.textContent = request.title;
@@ -44,7 +71,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             setTimeout(() => {
                 summaryContainer.style.opacity = "1";
             }, 100);
+
+            sendResponse({ status: "success", message: "Data processed successfully." });
         } else {
+            sendResponse({ status: "error", message: "Failed to process data." });
         }
     }
+});
+
+
+chrome.runtime.sendMessage({
+    action: "capture",
 });
