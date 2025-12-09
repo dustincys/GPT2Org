30377301050d725b5f0a0f041a62e0cf3f34bfe7
Yanshuo Chu <yanshuoc@gmail.com>
2025-12-09 09:43:59 +0800

    format option.js

--- options.js
+++ options.js
@@ -75,11 +75,11 @@ function save_options() {
         "useNewStyleLinks": NewStyleP,
         "toUseModel": toUseModel,
         "debug": debugP,
-    }, function () {
+    }, function() {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
         status.textContent = 'Options saved.';
-        setTimeout(function () {
+        setTimeout(function() {
             status.textContent = '';
         }, 750);
     });
@@ -89,7 +89,7 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
     // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get(null, function (options) {
+    chrome.storage.sync.get(null, function(options) {
         document.getElementById('ronProtocol').value = options.ronProtocol;
         document.getElementById('ronTemplate').value = options.ronTemplate;
         document.getElementById('rnnProtocol').value = options.rnnProtocol;
@@ -114,4 +114,3 @@ function restore_options() {
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
-
